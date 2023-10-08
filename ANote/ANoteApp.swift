2d33f60b6e6f363a71e489bb329c88ccf8f35726
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-08 17:06:09 +0300

    Initial Commit

---
+++ ANote/ANoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANoteApp.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ANoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
