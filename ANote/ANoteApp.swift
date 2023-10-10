5900a8976ba8acd14b0522cb696ace1013e3e909
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-10 21:48:56 +0300

    feat: model added for swiftdata

--- ANote/ANoteApp.swift
+++ ANote/ANoteApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ANoteApp: App {
+    let modelContainer : ModelContainer = {
+        let schema = Schema([Note.self,NoteBackground.self])
+        let configurations = ModelConfiguration(schema:schema)
+        let container = try! ModelContainer(for: schema, configurations: configurations)
+        return container
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(modelContainer)
         }
     }
 }
--- ANote/ContentView.swift
+++ ANote/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NoteListItem(note: Note(title: "sadsad", content: "sadasd", createdAt: Date(), background: NoteBackground.astronaut))
         }
         .padding()
     }
