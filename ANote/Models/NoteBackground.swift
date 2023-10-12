52a355d1436019f91008a8a22c60f579b8d4f8c9
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-12 21:34:33 +0300

    feat: model updated

--- ANote/Models/NoteBackground.swift
+++ ANote/Models/NoteBackground.swift
@@ -15,29 +15,13 @@ class NoteBackground{
     var id: Int
     let image: String?
     let color: String?
+    let textColor: String
     
-    init(id: Int, image: String? = nil, color: String? = nil) {
+    init(id: Int, image: String? = nil, color: String? = nil, textColor: String = "#ffffff") {
         self.id = id
         self.image = image
         self.color = color
+        self.textColor = textColor
     }
 }
 
-extension NoteBackground{
-    static let astronaut = NoteBackground(id: 1, image: "astronaut", color: nil)
-    static let autumn = NoteBackground(id: 2, image: "autumn", color: nil)
-    static let bengal = NoteBackground(id: 3, image: "bengal", color: nil)
-    static let cat = NoteBackground(id: 4, image: "cat", color: nil)
-    static let flowers = NoteBackground(id: 5, image: "flowers", color: nil)
-    static let lighthouse = NoteBackground(id: 6, image: "lighthouse", color: nil)
-    static let template = NoteBackground(id: 7, image: "template", color: nil)
-    static let tiger = NoteBackground(id: 8, image: "tiger", color: nil)
-    static let blue = NoteBackground(id: 9, image: nil, color: "blue")
-    static let red = NoteBackground(id: 10, image: nil, color: "red")
-    static let yellow = NoteBackground(id: 11, image: nil, color: "yellow")
-    static let pink = NoteBackground(id: 12, image: nil, color: "pink")
-    static let purple = NoteBackground(id: 13, image: nil, color: "purple")
-    static let orange = NoteBackground(id: 14, image: nil, color: "orange")
-    static let brown = NoteBackground(id: 15, image: nil, color: "brown")
-    static let mint = NoteBackground(id: 16, image: nil, color: "mint")
-}
