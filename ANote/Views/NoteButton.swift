a47774fdbd3f6a6e68c3914bda219b0817f95c7e
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-16 23:06:51 +0300

    feat: button added for noteItem

---
+++ ANote/Views/NoteButton.swift
@@ -0,0 +1,23 @@
+//
+//  NoteButton.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 15.10.2023.
+//
+
+import SwiftUI
+
+struct NoteButton: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .animation(.linear(duration: 0.1), value: configuration.isPressed)
+    }
+}
+
+#Preview {
+    Button(action: {}, label: {
+       NoteListItem(note: Note(title: "s", content: "s", createdAt: Date()),onLongPress: {},showSelectButton: false, isSelected: false)
+    })
+    .buttonStyle(NoteButton())
+}
--- ANote/Views/NoteItem/NoteListItem.swift
+++ ANote/Views/NoteItem/NoteListItem.swift
@@ -9,30 +9,66 @@ import SwiftUI
 
 struct NoteListItem: View {
     let note: Note
+    let onLongPress: () -> Void
+    let showSelectButton: Bool
+    let isSelected: Bool
     var body: some View {
-        VStack(spacing:10){
-            Text("Note Title")
-                .lineLimit(1)
-                .frame(maxWidth: .infinity,alignment: .leading)
-                .font(.title3)
-                .shadow(radius: 10)
-                .bold()
-            Text("Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet")
-                .frame(maxWidth: .infinity,alignment: .leading)
-                .font(.subheadline)
-                .lineLimit(4)
-                .shadow(color: .black, radius: 10)
-            Text("23.00")
-                .frame(maxWidth: .infinity,alignment: .leading)
-                .font(.footnote)
-        }
+        Button(action: {
+            
+        }, label: {
+            VStack(spacing:10){
+                Text("Note Title")
+                    .lineLimit(1)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .font(.title3)
+                    .shadow(radius: 10)
+                    .bold()
+                Text("Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet")
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .font(.subheadline)
+                    .lineLimit(4)
+                    .shadow(color: .black, radius: 10)
+                Text("23.00")
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .font(.footnote)
+            }
+            .padding()
+            .frame(maxWidth: .infinity,minHeight: 50,alignment: .topLeading)
+            .noteItemBackground(with: "1",isFullScreen: false)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
+            .overlay(alignment: .bottomTrailing){
+                if showSelectButton{
+                    VStack{
+                        if isSelected{
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color.white)
+                        .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .buttonStyle(.borderedProminent)
+                        .padding()
+                }
+            }
+        })
+        .simultaneousGesture(
+            LongPressGesture()
+                .onEnded { _ in
+                    onLongPress()
+                }
 
-        .frame(maxWidth: .infinity,minHeight: 50,alignment: .topLeading)
-        .padding()
-        .noteItemBackground(with: "0")
-        .foregroundStyle(.white)
-        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 20)))
-
+        )
+        .highPriorityGesture(
+            TapGesture()
+                .onEnded {
+                    if showSelectButton {
+                        onLongPress()
+                    }
+                }
+        )
+        .buttonStyle(NoteButton())
     }
 }
 
@@ -44,6 +80,9 @@ struct NoteListItem: View {
             createdAt: Date(),
             isPinned: true,
             background: Backgrounds.backgrounds[0]
-        )
+        ),
+        onLongPress: {},
+        showSelectButton: true,
+        isSelected: true
     )
 }
