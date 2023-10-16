1f83fdd2495ff3fb0b480b683ce8ec5d8bb927da
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-16 23:07:34 +0300

    feat: project updated

--- ANote/ContentView.swift
+++ ANote/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack{
-            VStack{
-                NavigationLink(destination: CreateThemeView(), label: {Text("sdsd")})
-            }
+            HomeView()
         }
     }
 }
