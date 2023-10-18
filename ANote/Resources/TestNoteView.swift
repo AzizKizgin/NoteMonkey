0a0d48017612159bc66ab24145b8963760fc5345
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-18 22:28:01 +0300

    feat: note view for test

---
+++ ANote/Resources/TestNoteView.swift
@@ -0,0 +1,30 @@
+//
+//  TestNoteView.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 18.10.2023.
+//
+
+import SwiftUI
+
+struct TestNoteView: View {
+    var body: some View {
+        NoteListItem(
+            note: Note(
+                title: "Note Title",
+                content: "Lorem ipsum dolor sit amet",
+                createdAt: Date(),
+                isPinned: true,
+                background: Backgrounds.backgrounds[0]
+            ),
+            onLongPress: {},
+            showSelectButton: true,
+            isSelected: true,
+            isListView: false
+        )
+    }
+}
+
+#Preview {
+    TestNoteView()
+}
