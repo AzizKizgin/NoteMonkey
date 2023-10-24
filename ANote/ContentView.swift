b91b1d5d6fe80134e233d86210c76dc313b1dc5e
Aziz Kızgın <azizkzgn@gmail.com>
2023-10-24 22:31:06 +0300

    feat: function added for saving default backgrounds

--- ANote/ContentView.swift
+++ ANote/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @AppStorage("isDefaultsSaved") var isAllReadySaved: Bool = false
     var body: some View {
         NavigationStack{
             HomeView()
+                .onAppear{
+                    if !isAllReadySaved {
+                        Backgrounds.backgrounds.forEach{ background in
+                            modelContext.insert(background)
+                        }
+                        isAllReadySaved.toggle()
+                    }
+                }
         }
     }
 }
