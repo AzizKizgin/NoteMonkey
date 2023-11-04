924498bb6c1d6f6ae11fb07f5cb2b57249118e42
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-04 22:57:04 +0300

    feat: toggle function added for isPinned

---
+++ ANote/Resources/Int.swift
@@ -0,0 +1,22 @@
+//
+//  Int.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 4.11.2023.
+//
+
+import Foundation
+
+extension Int{
+    mutating func toggle() {
+        if self == 1 {
+            self = 0
+            return
+        }
+        else if self == 0 {
+            self = 1
+            return
+        }
+        return
+    }
+}
