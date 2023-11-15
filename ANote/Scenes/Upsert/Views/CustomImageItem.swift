6a49cb6b5ac76d0e410064db5b3e9f326b32c445
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-15 17:34:08 +0300

    feat: CustomImageItem added

--- ANote/Scenes/Upsert/Views/CustomImageItem.swift
+++ ANote/Scenes/Upsert/Views/CustomImageItem.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct CustomImageItem: View {
+    let imageName: String
+    @State private var uiImage: UIImage?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            if let uiImage{
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 125,height: 200)
+                    .scaledToFill()
+            }
+            else {
+                ZStack{
+                    Color(hex: "default")
+                        .frame(width: 125,height: 200)
+                    ProgressView()
+                }
+            }
+        }
+        .onAppear{
+            if self.uiImage == nil {
+                if let cachedImage = ImageCache.shared.get(forKey: imageName){
+                    self.uiImage = cachedImage
+                }
+                else{
+                    ImageService.loadImage(imageName: imageName){ imageData in
+                        if let imageData, let uiImage = UIImage(data: imageData) {
+                            self.uiImage = uiImage
+                            ImageCache.shared.set(uiImage, forKey: imageName)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    CustomImageItem()
+    CustomImageItem(imageName: "")
 }
