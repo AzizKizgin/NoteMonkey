f3e5b669a5253f6943564d346c5e58dae9ea20b4
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-18 01:13:06 +0300

    feat: service updated

--- ANote/Utils/ImageService.swift
+++ ANote/Utils/ImageService.swift
@@ -18,7 +18,7 @@ struct ImageService{
         guard let image = image else {
             throw ImageError.imageDataConversion
         }
-        if let imageData = image.jpegData(compressionQuality: 1.0) {
+        if let imageData = image.jpegData(compressionQuality: 0.5) {
             let fileManager = FileManager.default
             let appName = "ANote"
             let imageUrl = "image_\(imageName).jpg"
@@ -31,6 +31,10 @@ struct ImageService{
                 }
                 let fileURL = appFolderURL.appendingPathComponent(imageUrl)
                 try imageData.write(to: fileURL)
+                if let uiImage = UIImage(data: imageData){
+                    ImageCache.shared.set(uiImage, forKey: imageName)
+                }
+                
             } catch {
                 throw ImageError.fileWrite
             }
@@ -39,52 +43,40 @@ struct ImageService{
         }
     }
     
-    static func loadImage(imageName: String, completion: @escaping (UIImage?) -> Void) {
+    static func loadImage(imageName: String) async -> UIImage? {
         guard !imageName.isEmpty else {
-            completion(nil)
-            return
+            return nil
         }
-        DispatchQueue.global(qos: .background).async {
-            do {
-                if  let cachedImage = ImageCache.shared.get(forKey: imageName){
-                    DispatchQueue.main.async {
-                        completion(cachedImage)
-                    }
-                    return
-                }
-                else {
-                    let fileManager = FileManager.default
-                    let appName = "ANote"
-                    let documentsDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                    let appFolderURL = documentsDirectory.appendingPathComponent(appName)
-                    guard fileManager.fileExists(atPath: appFolderURL.path) else {
-                        print("App folder does not exist")
-                        DispatchQueue.main.async {
-                            completion(nil)
-                        }
-                        return
-                    }
-                    let fileURL = appFolderURL.appendingPathComponent("image_\(imageName).jpg")
-                    guard fileManager.fileExists(atPath: fileURL.path) else {
-                        print("Image file 'image_\(imageName).jpg' does not exist")
-                        DispatchQueue.main.async {
-                            completion(nil)
-                        }
-                        return
-                    }
-                    let imageData = try Data(contentsOf: fileURL)
-                    DispatchQueue.main.async {
-                        let uIImage = UIImage(data: imageData)
-                        completion(uIImage)
-                    }
-                    return
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    completion(nil)
-                }
-                return
+        do {
+            if let cachedImage = ImageCache.shared.get(forKey: imageName){
+                return cachedImage
             }
+            else {
+                let fileManager = FileManager.default
+                let appName = "ANote"
+                let documentsDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                let appFolderURL = documentsDirectory.appendingPathComponent(appName)
+                guard fileManager.fileExists(atPath: appFolderURL.path) else {
+                    print("App folder does not exist")
+                    return nil
+                }
+                let fileURL = appFolderURL.appendingPathComponent("image_\(imageName).jpg")
+                guard fileManager.fileExists(atPath: fileURL.path) else {
+                    print("Image file 'image_\(imageName).jpg' does not exist")
+                    return nil
+                }
+                let imageData = try Data(contentsOf: fileURL)
+                if let uiImage = UIImage(data: imageData) {
+                    ImageCache.shared.set(uiImage, forKey: imageName)
+                    return uiImage
+                } else {
+                    return nil
+                }
+                
+            }
+        } catch {
+            return nil
         }
+        
     }
 }
