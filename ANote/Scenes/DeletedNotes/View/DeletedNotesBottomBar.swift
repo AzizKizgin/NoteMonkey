1a7b91cb1567c4f606f0f68d50b1072d1e6c1064
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-19 00:03:16 +0300

    feat: DeletedNotesBottomBar created

--- ANote/Scenes/DeletedNotes/View/DeletedNotesBottomBar.swift
+++ ANote/Scenes/DeletedNotes/View/DeletedNotesBottomBar.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct DeletedNotesBottomBar: View {
+    let onUnDelete: () -> Void
+    let deleteSelectedNotes: () -> Void
+    let selectedItemCount: Int
+    @State var showAlert: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:0){
+            Button(action: onUnDelete) {
+                Image(systemName: "arrowshape.turn.up.left.2")
+                    .font(.system(size: 25))
+                    .frame(maxWidth: .infinity)
+            }
+            Button(action: showDeleteAlert) {
+                Image(systemName: "trash")
+                    .font(.system(size: 25))
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .safeAreaPadding(20)
+        .background(.default)
+        .transition(.move(edge: .bottom).combined(with: .opacity))
+        .alert("Delete Note", isPresented: $showAlert) {
+            Button("Delete",role:.destructive) {
+                deleteSelectedNotes()
+            }
+            Button("Cancel", role: .cancel) {}
+        } message: {Text("^[\(selectedItemCount) note](inflect: true) will delete")}
+    }
+}
+
+extension DeletedNotesBottomBar{
+    private func showDeleteAlert(){
+        self.showAlert.toggle()
     }
 }
 
 #Preview {
-    DeletedNotesBottomBar()
+    DeletedNotesBottomBar(onUnDelete: {}, deleteSelectedNotes: {}, selectedItemCount: 11)
 }
