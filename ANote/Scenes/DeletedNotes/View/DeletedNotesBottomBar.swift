635b76d0e729a0f5d42cbba6b5fe5431d033f977
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-19 00:03:02 +0300

    feat: bottom bar created

---
+++ ANote/Scenes/DeletedNotes/View/DeletedNotesBottomBar.swift
@@ -0,0 +1,18 @@
+//
+//  DeletedNotesBottomBar.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 18.11.2023.
+//
+
+import SwiftUI
+
+struct DeletedNotesBottomBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeletedNotesBottomBar()
+}
