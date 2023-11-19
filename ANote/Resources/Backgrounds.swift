98ca66857271da51fab069edd2bf00ed8e5f956a
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-19 18:45:58 +0300

    feat: new background added

--- ANote/Resources/Backgrounds.swift
+++ ANote/Resources/Backgrounds.swift
@@ -19,13 +19,14 @@ struct Backgrounds {
         NoteBackground(id: "7", image: "template",createdAt: Date.now.addingTimeInterval(7*0.1)),
         NoteBackground(id: "8", image: "flower",createdAt: Date.now.addingTimeInterval(8*0.1)),
         NoteBackground(id: "9", image: "fish",createdAt: Date.now.addingTimeInterval(9*0.1)),
-        NoteBackground(id: "10", color: "#0579f5",createdAt: Date.now.addingTimeInterval(10*0.1)),
-        NoteBackground(id: "11", color: "#c40e0e",createdAt: Date.now.addingTimeInterval(11*0.1)),
-        NoteBackground(id: "12", color: "#fcba03",createdAt: Date.now.addingTimeInterval(12*0.1)),
-        NoteBackground(id: "13", color: "#ff14ff",createdAt: Date.now.addingTimeInterval(13*0.1)),
-        NoteBackground(id: "14", color: "#800db5",createdAt: Date.now.addingTimeInterval(14*0.1)),
-        NoteBackground(id: "15", color: "#fca503",createdAt: Date.now.addingTimeInterval(15*0.1)),
-        NoteBackground(id: "16", color: "#52390b",createdAt: Date.now.addingTimeInterval(16*0.1)),
-        NoteBackground(id: "17", color: "#3EB489",createdAt: Date.now.addingTimeInterval(17*0.1))
+        NoteBackground(id: "10", image: "snow",createdAt: Date.now.addingTimeInterval(10*0.1)),
+        NoteBackground(id: "11", color: "#0579f5",createdAt: Date.now.addingTimeInterval(11*0.1)),
+        NoteBackground(id: "12", color: "#c40e0e",createdAt: Date.now.addingTimeInterval(12*0.1)),
+        NoteBackground(id: "13", color: "#fcba03",createdAt: Date.now.addingTimeInterval(13*0.1)),
+        NoteBackground(id: "14", color: "#ff14ff",createdAt: Date.now.addingTimeInterval(14*0.1)),
+        NoteBackground(id: "15", color: "#800db5",createdAt: Date.now.addingTimeInterval(15*0.1)),
+        NoteBackground(id: "16", color: "#fca503",createdAt: Date.now.addingTimeInterval(16*0.1)),
+        NoteBackground(id: "17", color: "#52390b",createdAt: Date.now.addingTimeInterval(17*0.1)),
+        NoteBackground(id: "18", color: "#3EB489",createdAt: Date.now.addingTimeInterval(18*0.1))
         ]
 }
