bc699a4d02ec87025de2f0fbb830f738809acff0
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-19 18:44:42 +0300

    feat: new images added splash screen created

---
+++ ANote/Scenes/SplashScreen/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  ANote
+//
+//  Created by Aziz Kızgın on 19.11.2023.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
