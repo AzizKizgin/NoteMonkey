325cbe1e768d02804918d1a9a5ca7771cb71c2fb
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-19 18:46:10 +0300

    feat: splash screen

--- ANote/ContentView.swift
+++ ANote/ContentView.swift
@@ -16,24 +16,63 @@ struct ContentView: View {
     }) var backgrounds: [NoteBackground]
     @AppStorage("isDefaultsSaved") var isAllReadySaved: Bool = false
     @AppStorage("isDarkMode") var isDarkMode: Bool = false
+    @State private var showSplash: Bool = true
     var body: some View {
         NavigationStack{
-            HomeView()
-                .preferredColorScheme(isDarkMode ? .dark: .light)
-                .onAppear{
-                    DispatchQueue.global(qos: .background).async{
-                        if !isAllReadySaved {
-                            Backgrounds.backgrounds.forEach{ background in
-                                modelContext.insert(background)
+            if showSplash{
+                SplashScreen()
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1){
+                            withAnimation{
+                                showSplash.toggle()
                             }
-                            isAllReadySaved.toggle()
                         }
                     }
-                }
+            }
+            else{
+                HomeView()
+                    .onAppear{
+                        DispatchQueue.global(qos: .background).async{
+                            if !isAllReadySaved {
+                                Backgrounds.backgrounds.forEach{ background in
+                                    modelContext.insert(background)
+                                }
+                                isAllReadySaved.toggle()
+                            }
+                        }
+                    }
+            }
         }
+        .preferredColorScheme(isDarkMode ? .dark: .light)
     }
 }
 
 #Preview {
-    ContentView()
+    MainActor.assumeIsolated{
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: NoteBackground.self, Note.self, configurations: config)
+
+        let background = NoteBackground(id: "2", image: "bird",createdAt: Date.now.addingTimeInterval(2*100))
+        container.mainContext.insert(background)
+        
+        for index in 0...10 {
+            let note = Note(
+                title: "Note Title",
+                content: "Lorem ipsum dolor sit amet",
+                createdAt: Date()
+            )
+            if index % 2 == 0 {
+                note.isPinned = 1
+            }
+            note.background = background
+            container.mainContext.insert(note)
+        }
+        
+        return
+            NavigationStack{
+            ContentView()
+        }
+        .modelContainer(container)
+    }
+
 }
